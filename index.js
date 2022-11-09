186900c34830a4a79a93d2e115aa534b4ddb4a0d
sajthewizard <rsaj53@gmail.com>
2022-11-10 00:13:20 +0100

    service sorte

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 
@@ -26,6 +26,13 @@ async function run() {
             const cursor = servicecollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicecollection.findOne(query);
+            res.send(service);
+
         })
 
     }
